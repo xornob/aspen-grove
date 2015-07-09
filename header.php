45665f3e7259182a09e6aa80e5f5a2079048fab6
Matt Beall <mbeall@starverte.com>
2015-07-09 15:30:31 -0600

    Added header like Canvas theme

---
+++ header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  * The Header for our theme.
+ *   *
+ *    * Displays all of the <head> section and everything up till <div id="main">
+ *     *
+ *      * @package Flint\Aspen Grove
+ *       * @since 0.0.2
+ *        */
+get_header( 'head' );
+//get_header( 'branding' );
+//get_header( 'nav' ); ?>
